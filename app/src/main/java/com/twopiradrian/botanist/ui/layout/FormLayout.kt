22568dd0b450a99d4e36ea3a935c555edbc23916
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-02-06 01:15:43 -0300

    removing unused imports

--- app/src/main/java/com/twopiradrian/botanist/ui/layout/FormLayout.kt
+++ app/src/main/java/com/twopiradrian/botanist/ui/layout/FormLayout.kt
@@ -1,17 +1,13 @@
 package com.twopiradrian.botanist.ui.layout
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.twopiradrian.botanist.ui.components.input.rememberImeState
 
 @Composable
--- app/src/main/java/com/twopiradrian/botanist/ui/screens/register/RegisterScreen.kt
+++ app/src/main/java/com/twopiradrian/botanist/ui/screens/register/RegisterScreen.kt
@@ -1,13 +1,8 @@
 package com.twopiradrian.botanist.ui.screens.register
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -19,7 +14,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.twopiradrian.botanist.R
